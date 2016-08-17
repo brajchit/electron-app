6e73fbe5e8cfe344a8f340b874838e5878664720
Brannybk <brajchit@espol.edu.ec>
2016-08-17 12:00:45 -0500

    add bootstrap and menuvar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
+
+function fillOptions($selectTag, data) {
+    $(data).each(function(index, val) {
+        $('<option/>',{
+            value: val,
+            text : val
+        }).appendTo($selectTag);
+    });
+}
+
 $(document).ready(function() {
+
+    var equipos      = ["LASER", "OFFSET", "DUPLICADORA", "FOTOCOPIA"],
+        equiposVals  = [1.5, 2, 2.5, 3],
+        sustrato = ["BOND", "CANVAS", "PVC", "ADESIVOS"],
+        sustratoVals = [.30, .25, .40, .55],
+        arte     = ["AFICHE", "VOLANTE", "CARNE", "LIBRO", "FOLLETO", "TARJETA"],
+        arteVals = [.30, .25, .40, .55, .88, .35],
+        iva      = ["12%", "14%"],
+        ivaVals  = [.12, .14];
+
+    var $equiposSelects = $("select#equipo");
+    var $sustratoSelects = $("select#sustrato");
+    var $arteSelects = $("select#arte");
+    var $ivaSelects = $("select#iva");
+
+    fillOptions($equiposSelects, equipos);
+    fillOptions($sustratoSelects, sustrato);
+    fillOptions($arteSelects, arte);
+    fillOptions($ivaSelects, iva);
+
     $('select').each(function() {
         var $this = $(this),
             numberOfOptions = $(this).children('option').length;
@@ -6,6 +36,7 @@ $(document).ready(function() {
         $this.addClass('select-hidden');
         $this.wrap('<div class="select"></div>');
         $this.after('<div class="select-styled"></div>');
+        $this.parent().append('<label class="label-values" for="value">00.00</label>');
 
         var $styledSelect = $this.next('div.select-styled');
         $styledSelect.text($this.children('option').eq(0).text());
@@ -23,8 +54,7 @@ $(document).ready(function() {
 
         var $listItems = $list.children('li');
 
-
-
+        //dropdown list options
         $styledSelect.click(function(e) {
             if ($('.select-options').is(':visible')) {
                 e.stopPropagation();
@@ -43,12 +73,29 @@ $(document).ready(function() {
             } //end if
         });
 
+        //event when click on list option
         $listItems.click(function(e) {
             e.stopPropagation();
             $styledSelect.text($(this).text()).removeClass('active');
             $this.val($(this).attr('rel'));
             $list.hide();
-            //console.log($this.val());
+            console.log($this.val());
+
+            // cuando es LIBRO o FOLLETO
+            if ($(this).text() == "LIBRO" || $(this).text() == "FOLLETO" ) {
+                var warpArte = $('select#arte').parent();
+                $('body #npag, h2.LibroFollleto, select#color').remove();
+                warpArte.after('<h2 class="LibroFollleto">N. PAGINAS</h2><div class="select" id="npag"><input class="input-value" type="number" name="cantidad" min="1" step="1"></div>');
+                var $colorSelector = $('<select/>',{
+                    id: 'color'
+                }).append($('<option/>',{value:'hide'}).text("-- Select --"))
+                  .append($('<option/>',{value:'hide'}).text("COLOR"))
+                  .append($('<option/>',{value:'hide'}).text("BN"));
+                // warpArte.after($colorSelector);
+
+            } else {
+                $('body #npag, h2.LibroFollleto, select#color').remove()
+            }
         });
 
         $(document).click(function() {
